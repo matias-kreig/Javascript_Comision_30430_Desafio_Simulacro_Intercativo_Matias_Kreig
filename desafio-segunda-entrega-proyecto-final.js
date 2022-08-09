f5d7ed9b807bc6443def3580652d75f33d69cfdb
matias-kreig <matiaskreig1995@gmail.com>
2022-08-09 16:39:16 -0300

    Desafio Librerias

--- desafio-segunda-entrega-proyecto-final.js
+++ desafio-segunda-entrega-proyecto-final.js
@@ -108,7 +108,7 @@ function agregarAlCarro(cosaIndex){
             toast.addEventListener('mouseleave', Swal.resumeTimer)
         }
     })
-    toast.fire({
+    Toast.fire({
         icon: 'success',
         title: 'agregado al carrito'
     })
